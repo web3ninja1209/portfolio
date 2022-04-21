b7b8a294ac2b5916209eab284a8ec9ba1972892b
Rajat Ratewal <rajatratewal@gmail.com>
2022-04-21 16:23:59 +0530

    added blog

--- src/data.js
+++ src/data.js
@@ -3,20 +3,21 @@ import fab from "../src/img/fab.png";
 import ninja from "../src/img/ninja.jpeg";
 import devpost from "../src/img/devpost.png";
 import climate from "../src/img/climate.jpeg";
-import hack from "../src/img/hack.png";
+import hack from "../src/img/hack.jpeg";
 import firebase from "../src/img/firebase.png";
 import nft from "../src/img/nft.png";
 import book from "../src/img/book.jpeg";
 import game from "../src/img/game.jpeg";
 import opensea from "../src/img/opensea.jpeg";
+import blog from "../src/img/blog.png";
 
 
 export const products = [
  
   {
-    id: 1,
-    img: youtube,
-    link: "https://www.youtube.com/channel/UChwdfzEgOtpadYTXEcKedNQ",
+    id:1,
+    img: hack,
+    link: "https://decentralandescrow.herokuapp.com/",
   },
   {
     id: 2,
@@ -25,8 +26,8 @@ export const products = [
   },
   {
     id: 3,
-    img: hack,
-    link: "https://decentralandescrow.herokuapp.com/",
+    img: blog,
+    link: "https://yogafireblog.herokuapp.com/",
   },
   {
     id: 4,
@@ -40,31 +41,36 @@ export const products = [
   },
   {
     id: 6,
+    img: youtube,
+    link: "https://www.youtube.com/channel/UChwdfzEgOtpadYTXEcKedNQ",
+  }, 
+  {
+    id: 7,
     img:ninja,
     link: "https://www.udemy.com/course/aws-ninja",
   },
   {
-    id: 7,
+    id: 8,
     img:devpost,
     link: "https://devpost.com/software/marketplace-bot",
   },
   {
-    id: 8,
+    id: 9,
     img: firebase,
     link: "https://commplatform-e0d04.firebaseapp.com/",
   },
   {
-    id: 9,
+    id: 10,
     img: book,
     link: "https://www.amazon.in/Simply-Cell-Rajat-Ratewal-ebook/dp/B086RP93SW",
   },
   {
-    id: 10,
+    id: 11,
     img: climate,
     link: "https://yogafireguru.herokuapp.com/",
   },
   {
-    id: 11,
+    id: 12,
     img: fab,
     link: "http://fabacademy.org/2019/labs/akgec/students/rajat-ratewal/projects/projectpresentation/",
   },
